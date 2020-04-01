d269ef879fa32032aa58e293e9036ca5445aefd1
Jerry Reinoehl <jerryreinoehl@gmail.com>
2020-04-01 15:57:23 -0600

    Added comments and usage.

--- update.py
+++ update.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
 ################################################################################
 # update.py
+#
+# Fetches files from git repository and extracts them to current directory.
+#
+# usage: update.py
+#
 ################################################################################
 
 
@@ -11,6 +16,7 @@ import requests
 import zipfile
 
 
+# Location of git repository.
 ARCHIVE_URL = 'https://github.com/jerryreinoehl/cisx/archive/master.zip'
 
 
@@ -22,21 +28,26 @@ def main():
         print(f'response returned with status {response.status_code}')
         exit(1)
 
+    # write response content to zip file
     with open('tmp', 'wb') as zip_file:
         zip_file.write(response.content)
 
+    # extract zip file to current directory
     with zipfile.ZipFile('tmp', 'r') as zip_file:
         archive_name = zip_file.infolist()[0].filename
         zip_file.extractall()
 
     walk_dir(archive_name, lambda file: print(f'extracting: {file}'))
 
+    # copy or overwrite from archive files
     distutils.dir_util.copy_tree(archive_name, './')
 
+    # remove temporary zip file and archive files
     os.remove('tmp')
     distutils.dir_util.remove_tree(archive_name)
 
 
+# Recursively visits each file and directory within dir.
 def walk_dir(dir, visit):
     if not os.path.exists(dir):
         return
